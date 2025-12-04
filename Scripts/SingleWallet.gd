1ff5678f9c64489cc84af58d62b61bfd2ddf383d
Jackc28Git <jack.a.chastain@protonmail.com>
2025-12-04 17:30:26 -0600

    1.2 Single Wallet Node
    Added the single wallet node, a node that manages one wallet when you don't need the features of the WalletManager.

---
+++ Scripts/SingleWallet.gd
@@ -0,0 +1,29 @@
+@icon("res://addons/Wallet-Node/Icons/SingleWallet.svg")
+
+extends Node
+class_name SingleWallet ## A node for managing a single wallet when the wallet manager is too much,
+
+# VARIABLES
+
+@export var wallet: Wallet
+@export_group("Wallet Values")
+@export var currency_name: String: ## The name of the currency contained by the wallet
+	get:
+		return wallet.currency_name
+	set(x):
+		wallet.currency_name = x
+@export var amount: float = 0: ## The amount of currency in the wallet.
+	get:
+		return wallet.amount
+	set(x):
+		wallet.amount = x
+@export var max_amount: float: ## The maximum amount of currency that can be contained in the wallet
+	get:
+		return wallet.max_amount
+	set(x):
+		wallet.max_amount = x
+@export var enable_debt: bool: ## Allows the wallets amount to become negative. The minimum will just be the the negative maximum.
+	get:
+		return wallet.enable_debt
+	set(x):
+		wallet.enable_debt = x
